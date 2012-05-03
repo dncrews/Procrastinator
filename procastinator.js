d48c628b1b3c723fb3715c3018ea6e4d980849eb
Dan Crews <crewsd@gmail.com>
2012-05-02 21:28:19 -0700

    Restarting with twilio library

--- procastinator.js
+++ procastinator.js
@@ -1,91 +1,19 @@
-var twilio = require('twilio'),
-	twilioClient = twilio.Client,
-	twiml = twilio.Twiml,
+var twilioClient = require('twilio').Client,
+	client = new twilioClient('ACa09b866711055b9ddad8c1d5adc2c1c7','a4292a6c67e9be8367598054344523ba','http://fierce-snow-2861.herokuapp.com/'),
+	phone = client.getPhoneNumber('+18017018979'),
 	express = require('express'),
-	app = express.createServer();
-	client = new twilioClient('ACa09b866711055b9ddad8c1d5adc2c1c7', 'a4292a6c67e9be8367598054344523ba', 'http://4ipm.localtunnel.com'),
-	phone = client.getPhoneNumber('+14155992671')
-
+	app = express.createServer()
 
 phone.setup(function(){
-	// phone.on('incomingSms', function(reqParams, res) {
-	// 	console.log(reqParams.Body)
-	// 	console.log(reqParams)
-	// })
+	phone.on('incomingSms', function(reqParams, res) {
+		console.log(reqParams)
+		console.log(res)
+	})
 })
 
-app.listen(8000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/', function(req,res) {
+	res.send('test')
+})
+
+app.listen(8080)
 
