bcf0278d8e1bf7ec269d481e3edc1459dab50765
Dan Crews <crewsd@gmail.com>
2012-05-02 21:52:22 -0700

    Working?

--- procastinator.js
+++ procastinator.js
@@ -1,6 +1,6 @@
 var twilioClient = require('twilio').Client,
 	port = process.env.PORT || 8080
-	client = new twilioClient('ACa09b866711055b9ddad8c1d5adc2c1c7','a4292a6c67e9be8367598054344523ba','http://fierce-snow-2861.herokuapp.com/', {"port" : port}),
+		client = new twilioClient('ACa09b866711055b9ddad8c1d5adc2c1c7','a4292a6c67e9be8367598054344523ba','http://fierce-snow-2861.herokuapp.com/', {"port" : port}),
 	phone = client.getPhoneNumber('+18017018979'),
 	express = require('express'),
 	app = express.createServer()
@@ -9,6 +9,7 @@ phone.setup(function(){
 	phone.on('incomingSms', function(reqParams, res) {
 		console.log(reqParams)
 		console.log(res)
+		res.send('test')
 	})
 })
 
